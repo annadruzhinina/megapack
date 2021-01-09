1953c0eb06e8ce085a5fa149d52edc67cecceb6f
anna <anna@rcbd.org>
2021-01-08 22:07:44 -0500

    #7 Hero component

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import Hero from "./Hero";
 
 function App() {
   return (
-    <div>
-      <Button text={'String for test'}/>
+    <React.Fragment>
+      {/*<Button text={'String for test'}/>
       <Button text={'String_2 for test'} btnStyle={'lg'}/>
       <Button text={'String_3 for test'} btnStyle={'shadow'}/>
-      <Button text={'String_4 for test'} btnStyle={'md'}/>
+      <Button text={'String_4 for test'} btnStyle={'md'}/>*/}
       <Hero/>
-    </div>
+    </React.Fragment>
   );
 }
 
